85bf7a3bffffe41d761d29b56ddefe422230ba1b
Blake <blakewilliamhome@gmail.com>
2024-04-18 19:56:41 -0700

    Header & Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,13 @@
 <template>
-    Footer here
+    <footer class="bg-gray-200 border-t text-center border-gray-300 p-8">
+            <div class="mb-6 text-lg">
+                <nuxt-link to="/">Esterline Audio</nuxt-link>
+            </div>
+            <nav class="flex flex-row justify-center gap-4">
+                <nuxt-link to="page1">page1</nuxt-link>
+                <nuxt-link to="page2">page2</nuxt-link>
+                <nuxt-link to="page3">page3</nuxt-link>
+                <nuxt-link to="page4">page4</nuxt-link>
+            </nav>
+    </footer>
 </template>
--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,13 @@
 <template>
-    Header & Nav here
+    <header class="bg-sky-500 py-2">
+        <main class=" container mx-auto flex flex-row justify-between">
+            <div class="text-white text-lg font-bold"><nuxt-link to="/">Esterline Audio</nuxt-link></div>
+            <nav class="flex flex-row justify-end gap-4">
+                <nuxt-link to="page1">page1</nuxt-link>
+                <nuxt-link to="page2">page2</nuxt-link>
+                <nuxt-link to="page3">page3</nuxt-link>
+                <nuxt-link to="page4">page4</nuxt-link>
+            </nav>
+        </main>
+    </header>
 </template>
