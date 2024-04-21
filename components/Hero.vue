62561dc315b0ea9d805bf29eefeca69540581661
Blake <blakewilliamhome@gmail.com>
2024-04-21 16:33:04 -0700

    more images

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,102 +1,97 @@
 <template>
-<section class="w-fill mx-auto flex flex-row h-200 items-center bg-image pt-44 pb-24 bg-image px-8 ">
+  <section class="w-fill mx-auto flex flex-col sm:flex-row h-200 items-center bg-image pt-40 pb-28 bg-image px-8">
     <!-- Image container with background -->
-      <div class="mr-4 w-fit h-inherit text-center sm:w-1/2 sm:text-left bg-black px-5 py-5 rounded-md bg-opacity-75 text-opacity-0">
-        <h1 class="mb-6 text-3xl font-bold leading-tight text-white md:text-4xl">
-          Welcome to Esterline Audio!
-        </h1>
-        <p class="mb-2 leading-relaxed text-gray-300">
-          This is my website to showcase my previous work & offer my services. I've been in the audio industry for 3 years now and have a degree in Audio Production from Northwest University where I worked on many professional level projects to hone my skills in the industry.
-        </p>
-        <ul class="mb-8 flex flex-col items-center space-y-1 text-gray-300 sm:items-start">
-            <li class="flex items-end">
-              <svg
-                preserveAspectRatio="xMidYMid meet"
-                viewBox="0 0 24 24"
-                width="1.2em"
-                height="1.2em"
-                class="mr-2 text-verdun-green-300"
-              >
-                <path
-                  fill="none"
-                  stroke="currentColor" 
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m5 12l5 5L20 7"
-                ></path>
-              </svg>
-              <span>Various types of coffee beans</span>
-            </li>
-            <li class="flex items-end">
-              <svg
-                preserveAspectRatio="xMidYMid meet"
-                viewBox="0 0 24 24"
-                width="1.2em"
-                height="1.2em"
-                class="mr-2 text-verdun-green-300"
-              >
-                <path
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m5 12l5 5L20 7"
-                ></path>
-              </svg>
-              <span>Coworking area</span>
-            </li>
-            <li class="flex items-end">
-              <svg
-                preserveAspectRatio="xMidYMid meet"
-                viewBox="0 0 24 24"
-                width="1.2em"
-                height="1.2em"
-                class="mr-2 text-verdun-green-300"
-              >
-                <path
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m5 12l5 5L20 7"
-                ></path>
-              </svg>
-              <span>Meeting room</span>
-            </li>
-          </ul>
-          <div class="flex flex-col space-y-3 md:flex-row md:space-x-2 md:space-y-0">
-            <button
-              class="rounded-lg border-0 bg-verdun-green-900 px-6 py-3 text-base text-white shadow-lg transition hover:bg-verdun-green-300 hover:text-gray-900"
-            >
-              Open menu
-            </button>
-            <nuxt-link to="/services">
-              <button
-                class="rounded-lg border-0 bg-white px-6 py-3 text-base text-verdun-green-900 shadow-lg transition hover:bg-verdun-green-300 hover:text-gray-900"
-              >
-                Explore services
-              </button>
-            </nuxt-link>
-          </div>
+    <div class="mr-4 w-fit h-inherit text-left sm:w-1/2 bg-black px-5 py-5 rounded-md bg-opacity-75 text-opacity-0">
+      <h1 class="mb-6 text-3xl font-bold leading-tight text-white md:text-4xl">
+        Welcome to Esterline Audio!
+      </h1>
+      <p class="mb-2 leading-relaxed text-gray-300">
+        This is my website to showcase my previous work & offer my services. I've been in the audio industry for 3 years now and have a degree in Audio Production from Northwest University where I worked on many professional level projects to hone my skills in the industry.
+      </p>
+      <ul class="mb-8 flex flex-col items-center space-y-1 text-gray-300 sm:items-start">
+        <li class="flex items-end">
+          <svg
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+            width="1.2em"
+            height="1.2em"
+            class="mr-2 text-verdun-green-300"
+          >
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m5 12l5 5L20 7"
+            ></path>
+          </svg>
+          <span>Various types of coffee beans</span>
+        </li>
+        <li class="flex items-end">
+          <svg
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+            width="1.2em"
+            height="1.2em"
+            class="mr-2 text-verdun-green-300"
+          >
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m5 12l5 5L20 7"
+            ></path>
+          </svg>
+          <span>Coworking area</span>
+        </li>
+        <li class="flex items-end">
+          <svg
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+            width="1.2em"
+            height="1.2em"
+            class="mr-2 text-verdun-green-300"
+          >
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m5 12l5 5L20 7"
+            ></path>
+          </svg>
+          <span>Meeting room</span>
+        </li>
+      </ul>
+      <div class="flex flex-col space-y-3 md:flex-row md:space-x-2 md:space-y-0">
+<button class="disabled:opacity-50 disabled:cursor-not-allowed whitespace-nowrap w-full py-2 px-5 border-b border-white rounded-md shadow-sm hover:bg-white hover:text-black text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white">
+          Learn More
+        </button>
+        <button class="disabled:opacity-50 disabled:cursor-not-allowed whitespace-nowrap w-full py-2 px-5 border-b border-white rounded-md shadow-sm hover:bg-white hover:text-black text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white">
+          Contact Us
+        </button>
       </div>
-      <div class="mr-4 w-fit h-inherit text-center sm:w-1/2 sm:text-left bg-black px-5 py-5 rounded-md bg-opacity-75 text-opacity-0">
-        <img src="@/components/images/EAPSFullW.png" alt="">
-      </div>
-    <!-- Text container -->
-    
-    </section>
+    </div>
+
+  
+  <img
+    class="w-60 h-60 object-cover justify-right"
+    src="@/components/images/EAPSFullW.png"
+    alt="background"
+  />
+
+  </section>
 </template>
 
 <style scoped>
-  body {
-    background-color: #1f2937; /* Dark background color */
-  }
-  .bg-image {
-    background-image: url('@/components/images/bridge.jpg');
-    background-size: cover;
-    background-position: center;
-  }
+.bg-image {
+  background-image: url(@/components/images/bridge.jpg);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,4 @@
-<template>
+<template class="bg-gray-800">
     <Hero />
     <div class="bg-gray-800 text-center text-white">
         <div class="text-4xl font-bold pt-10">What I Do</div>
@@ -104,6 +104,48 @@
     <p class="leading-relaxed text-slate-700 dark:text-slate-400">
         I'm a fresh face in the audio world, eager to dedicate all my energy to your project. With a focus on personalized service, I'm here to cater to your specific needs. While I'm building my portfolio, your project will receive my full attention and commitment. Let's create something great together!
     </p>
+  </div>
+</section>
+<section
+  class="container mx-auto flex flex-col items-center px-12 py-28 sm:flex-row-reverse"
+>
+  <img
+    alt="Security"
+    class="mb-10 h-24 w-full pr-16 pl-16 sm:mb-0 sm:h-fit sm:w-1/2 sm:pl-24 sm:pr-0"
+    src="@/components/images/band.png"
+  />
+  <div class="mr-4 w-full text-center sm:w-1/2 sm:text-left">
+    <h2
+      class="mb-4 text-3xl font-bold leading-tight dark:text-slate-50 md:text-4xl"
+    >
+      I'm Passionate About Music
+    </h2>
+    <p class="leading-relaxed text-slate-700 dark:text-slate-400">
+      I love making music! As a musician, I have played in  various bands and ensembles throughout my life...
+    </p>
+    <a
+      class="group relative mx-auto mt-8 inline-block font-semibold text-slate-900 underline decoration-violet-300 decoration-2 underline-offset-8 transition hover:decoration-slate-700 dark:text-slate-200 dark:hover:decoration-slate-200 sm:mx-0"
+      href="#"
+    >
+      Supported authentication
+      
+      <svg
+        preserveAspectRatio="xMidYMid meet"
+        viewBox="0 0 24 24"
+        width="1.2em"
+        height="1.2em"
+        class="absolute bottom-0 -right-6 scale-x-0 transition group-hover:scale-x-100"
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M5 12h14m-4 4l4-4m-4-4l4 4"
+        ></path>
+      </svg>
+    </a>
   </div>
 </section>
 </template>
