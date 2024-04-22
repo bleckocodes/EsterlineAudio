97154d9dad9bbfb00f12bdb0bb55e63bafd4e5a4
Blake <blakewilliamhome@gmail.com>
2024-04-21 23:28:50 -0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,66 +5,27 @@
   </div>
   <!--Start Content-->
 
-
-
-  <section class=" flex flex-col items-center bg-gray-800 sm:py-10 sm:flex-row">
-<img
-  alt="Industry Experience"
-  class="mb-10 object-cover h-96 w-full sm:mb-0 sm:w-1/2 sm:pr-8"
-  src="@/components/images/PhotoWorking.jpg"
-/>
-<div class="w-full text-center sm:w-1/2 sm:text-left">
-  <h2 class="mb-4 text-3xl font-bold leading-tight text-slate-50 md:text-4xl">
-    Industry Experience
-  </h2>
-  <p class="mb-6 leading-relaxed text-slate-300">
-    I have experience with the latest industry standards of mixing and mastering audio for film, television, and video.
-  </p>
-  <a
-    class="group relative inline-block font-semibold underline decoration-verdun-green decoration-2 underline-offset-8 transition text-slate-200 hover:decoration-slate-200 sm:mx-0"
-    href="/Services"
-  >
-    View My Work
-    <svg
-      preserveAspectRatio="xMidYMid meet"
-      viewBox="0 0 24 24"
-      width="1.2em"
-      height="1.2em"
-      class="absolute bottom-0 -right-6 scale-x-0 transition group-hover:scale-x-100"
-    >
-      <path
-        fill="none"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M5 12h14m-4 4l4-4m-4-4l4 4"
-      ></path>
-    </svg>
-  </a>
-</div>
-</section>
   <section class=" flex flex-col items-center bg-gray-800 sm:flex-row-reverse"
 >
 <img
   alt="Security"
-  class="mb-10 object-cover h-96 w-full sm:mb-0 sm:w-1/2 sm:pl-8"
-  src="@/components/images/NAMM.jpg"
+  class="mb-10 object-cover h-128 w-1/2 sm:pl-8"
+  src="@/components/images/PhotoWorking.jpg"
 />
 <div class="mr-4 w-full text-center sm:w-1/2 sm:text-left">
   <h2
     class="mb-4 text-3xl font-bold leading-tight text-slate-50 md:text-4xl"
   >
-    Industry Standard Experience
+    Industry Experience
   </h2>
   <p class="leading-relaxed text-slate-300">
-    I have experience with the latest industry standards of mixing and mastering audio for film, television, video.
+    I have extensive experience with the latest industry standards of mixing and mastering audio for film, television, video.
   </p>
   <a
     class="group relative mx-auto mt-8 inline-block font-semibold underline decoration-verdun-green decoration-2 underline-offset-8 transition text-slate-200 hover:decoration-slate-200 sm:mx-0"
-    href="#"
+    href="/MyWork"
   >
-        About Me      
+      View My Work      
     <svg
       preserveAspectRatio="xMidYMid meet"
       viewBox="0 0 24 24"
@@ -88,7 +49,7 @@
 >
 <img
   alt="Security"
-  class="mb-10 object-cover h-128 w-full sm:mb-0 sm:w-1/2 sm:pr-8"
+  class="mb-10 object-cover h-128 w-1/2 sm:pr-8"
   src="@/components/images/Personal.jpg"
 />
 <div class="mr-4 w-full text-center sm:w-1/2 sm:text-left">
@@ -100,13 +61,35 @@
   <p class="leading-relaxed text-slate-300">
       I'm a fresh face in the audio world, eager to dedicate all my energy to your project. With a focus on personalized service, I'm here to cater to your specific needs. While I'm building my portfolio, your project will receive my full attention and commitment. Let's create something great together!
   </p>
+  <a
+    class="group relative mx-auto mt-8 inline-block font-semibold underline decoration-verdun-green decoration-2 underline-offset-8 transition text-slate-200 hover:decoration-slate-200 sm:mx-0"
+    href="/Services"
+  >
+      My Services      
+    <svg
+      preserveAspectRatio="xMidYMid meet"
+      viewBox="0 0 24 24"
+      width="1.2em"
+      height="1.2em"
+      class="absolute bottom-0 -right-6 scale-x-0 transition group-hover:scale-x-100"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M5 12h14m-4 4l4-4m-4-4l4 4"
+      ></path>
+    </svg>
+  </a>
 </div>
 </section>
   <section class=" flex flex-col items-center bg-gray-800 sm:flex-row-reverse"
 >
 <img
   alt="Security"
-  class="mb-10 object-cover h-96 w-full sm:mb-0 sm:w-1/2 sm:pl-8"
+  class="mb-10 object-cover h-128 w-1/2 sm:pl-8"
   src="@/components/images/band.png"
 />
 <div class="mr-4 w-full text-center sm:w-1/2 sm:text-left">
