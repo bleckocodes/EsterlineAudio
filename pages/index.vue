feb23c5e39de3ac7283764a1b0aa51e6ef1f0655
Blake <blakewilliamhome@gmail.com>
2024-04-21 22:51:23 -0700

    I hate this

--- pages/index.vue
+++ pages/index.vue
@@ -1,149 +1,147 @@
 <template class="bg-gray-800">
-    <Hero />
-    <div class="bg-gray-800 text-center text-white">
-        <div class="text-5xl font-bold pt-36 pb-10">Why You Should Work with Me</div>
-    </div>
+  <Hero />
+  <div class="bg-gray-800 text-center text-white">
+      <div class="text-5xl font-bold pt-36 pb-40">Why You Should Work with Me</div>
+  </div>
+  <!--Start Content-->
+
+
+
+  <section class=" flex flex-col items-center bg-gray-800 sm:py-10 sm:flex-row">
+<img
+  alt="Industry Experience"
+  class="mb-10 object-cover h-96 w-full sm:mb-0 sm:w-1/2 sm:pr-8"
+  src="@/components/images/PhotoWorking.jpg"
+/>
+<div class="w-full text-center sm:w-1/2 sm:text-left">
+  <h2 class="mb-4 text-3xl font-bold leading-tight text-slate-50 md:text-4xl">
+    Industry Experience
+  </h2>
+  <p class="mb-6 leading-relaxed text-slate-300">
+    I have experience with the latest industry standards of mixing and mastering audio for film, television, and video.
+  </p>
+  <a
+    class="group relative inline-block font-semibold underline decoration-verdun-green decoration-2 underline-offset-8 transition text-slate-200 hover:decoration-slate-200 sm:mx-0"
+    href="/Services"
+  >
+    View My Work
+    <svg
+      preserveAspectRatio="xMidYMid meet"
+      viewBox="0 0 24 24"
+      width="1.2em"
+      height="1.2em"
+      class="absolute bottom-0 -right-6 scale-x-0 transition group-hover:scale-x-100"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M5 12h14m-4 4l4-4m-4-4l4 4"
+      ></path>
+    </svg>
+  </a>
+</div>
+</section>
+  <section class=" flex flex-col items-center bg-gray-800 sm:flex-row-reverse"
+>
+<img
+  alt="Security"
+  class="mb-10 object-cover h-96 w-full sm:mb-0 sm:w-1/2 sm:pl-8"
+  src="@/components/images/NAMM.jpg"
+/>
+<div class="mr-4 w-full text-center sm:w-1/2 sm:text-left">
+  <h2
+    class="mb-4 text-3xl font-bold leading-tight text-slate-50 md:text-4xl"
+  >
+    Industry Standard Experience
+  </h2>
+  <p class="leading-relaxed text-slate-300">
+    I have experience with the latest industry standards of mixing and mastering audio for film, television, video.
+  </p>
+  <a
+    class="group relative mx-auto mt-8 inline-block font-semibold underline decoration-verdun-green decoration-2 underline-offset-8 transition text-slate-200 hover:decoration-slate-200 sm:mx-0"
+    href="#"
+  >
+        About Me      
+    <svg
+      preserveAspectRatio="xMidYMid meet"
+      viewBox="0 0 24 24"
+      width="1.2em"
+      height="1.2em"
+      class="absolute bottom-0 -right-6 scale-x-0 transition group-hover:scale-x-100"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M5 12h14m-4 4l4-4m-4-4l4 4"
+      ></path>
+    </svg>
+  </a>
+</div>
+</section>
+  <section class=" flex flex-col items-center bg-gray-800 sm:flex-row"
+>
+<img
+  alt="Security"
+  class="mb-10 object-cover h-128 w-full sm:mb-0 sm:w-1/2 sm:pr-8"
+  src="@/components/images/Personal.jpg"
+/>
+<div class="mr-4 w-full text-center sm:w-1/2 sm:text-left">
+  <h2
+    class="mb-4 text-3xl font-bold leading-tight text-slate-50 md:text-4xl"
+  >
+  Personalized Service for Every Project
+  </h2>
+  <p class="leading-relaxed text-slate-300">
+      I'm a fresh face in the audio world, eager to dedicate all my energy to your project. With a focus on personalized service, I'm here to cater to your specific needs. While I'm building my portfolio, your project will receive my full attention and commitment. Let's create something great together!
+  </p>
+</div>
+</section>
+  <section class=" flex flex-col items-center bg-gray-800 sm:flex-row-reverse"
+>
+<img
+  alt="Security"
+  class="mb-10 object-cover h-96 w-full sm:mb-0 sm:w-1/2 sm:pl-8"
+  src="@/components/images/band.png"
+/>
+<div class="mr-4 w-full text-center sm:w-1/2 sm:text-left">
+  <h2
+    class="mb-4 text-3xl font-bold leading-tight text-slate-50 md:text-4xl"
+  >
+    I'm Passionate About Music
+  </h2>
+  <p class="leading-relaxed text-slate-300">
+    I love making music! As a musician, I have played in various bands and ensembles throughout my life...
+  </p>
+  <a
+    class="group relative mx-auto mt-8 inline-block font-semibold underline decoration-verdun-green decoration-2 underline-offset-8 transition  text-slate-200 hover:decoration-slate-200 sm:mx-0"
+    href="#"
+  >
+    About Me
     
-    <section class="mx-auto flex flex-col items-center bg-gray-800 py-28 sm:flex-row" >
-  <img
-    alt="Inndustry Experience"
-    class="mb-10 h-24 w-full pr-16 pl-16 sm:mb-0 sm:h-fit sm:w-1/2 sm:pr-24 sm:pl-0"
-    src="@/components/images/PhotoWorking.jpg"
-  />
-  <div class="mr-4 w-full text-center sm:w-1/2 sm:text-left">
-    <h2
-      class="mb-4 text-3xl font-bold leading-tight text-slate-50 md:text-4xl"
+    <svg
+      preserveAspectRatio="xMidYMid meet"
+      viewBox="0 0 24 24"
+      width="1.2em"
+      height="1.2em"
+      class="absolute bottom-0 -right-6 scale-x-0 transition group-hover:scale-x-100"
     >
-      Industry Experience
-    </h2>
-    <p class="leading-relaxed text-slate-300">
-        I have experience with the latest industry standards of mixing  and mastering audio for film, television, video.
-    </p>
-    <a
-      class="group relative mx-auto mt-8 inline-block font-semibold underline decoration-verdun-green decoration-2 underline-offset-8 transition text-slate-200 hover:decoration-slate-200 sm:mx-0"
-      href="/Services"
-    >
-      View My Work
-      
-      <svg
-        preserveAspectRatio="xMidYMid meet"
-        viewBox="0 0 24 24"
-        width="1.2em"
-        height="1.2em"
-        class="absolute bottom-0 -right-6 scale-x-0 transition group-hover:scale-x-100"
-      >
-        <path
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M5 12h14m-4 4l4-4m-4-4l4 4"
-        ></path>
-      </svg>
-    </a>
-  </div>
+      <path
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M5 12h14m-4 4l4-4m-4-4l4 4"
+      ></path>
+    </svg>
+  </a>
+</div>
 </section>
-<section
-  class="mx-auto flex flex-col bg-gray-800 items-center py-28 sm:flex-row-reverse"
->
-  <img
-    alt="Security"
-    class="mb-10 h-24 w-full pr-16 pl-16 sm:mb-0 sm:h-fit sm:w-1/2 sm:pl-24 sm:pr-0"
-    src="@/components/images/NAMM.jpg"
-  />
-  <div class="mr-4 w-full text-center sm:w-1/2 sm:text-left">
-    <h2
-      class="mb-4 text-3xl font-bold leading-tight text-slate-50 md:text-4xl"
-    >
-      Industry Standard Experience
-    </h2>
-    <p class="leading-relaxed text-slate-300">
-      I have experience with the latest industry standards of mixing  and mastering audio for film, television, video.
-    </p>
-    <a
-      class="group relative mx-auto mt-8 inline-block font-semibold underline decoration-verdun-green decoration-2 underline-offset-8 transition text-slate-200 hover:decoration-slate-200 sm:mx-0"
-      href="#"
-    >
-          About Me      
-      <svg
-        preserveAspectRatio="xMidYMid meet"
-        viewBox="0 0 24 24"
-        width="1.2em"
-        height="1.2em"
-        class="absolute bottom-0 -right-6 scale-x-0 transition group-hover:scale-x-100"
-      >
-        <path
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M5 12h14m-4 4l4-4m-4-4l4 4"
-        ></path>
-      </svg>
-    </a>
-  </div>
-</section>
-<section
-  class="mx-auto flex flex-col bg-gray-800 items-center py-28 sm:flex-row"
->
-  <img
-    alt="Security"
-    class="mb-10 h-24 w-full pr-16 pl-16 sm:mb-0 sm:h-fit sm:w-1/2 sm:pr-24 sm:pl-0"
-    src="@/components/images/Personal.jpg"
-  />
-  <div class="mr-4 w-full text-center sm:w-1/2 sm:text-left">
-    <h2
-      class="mb-4 text-3xl font-bold leading-tight text-slate-50 md:text-4xl"
-    >
-    Personalized Service for Every Project
-    </h2>
-    <p class="leading-relaxed text-slate-300">
-        I'm a fresh face in the audio world, eager to dedicate all my energy to your project. With a focus on personalized service, I'm here to cater to your specific needs. While I'm building my portfolio, your project will receive my full attention and commitment. Let's create something great together!
-    </p>
-  </div>
-</section>
-<section
-  class="mx-auto flex flex-col items-center bg-gray-800 py-28 sm:flex-row-reverse"
->
-  <img
-    alt="Security"
-    class="mb-10 h-24 w-full pr-16 pl-16 sm:mb-0 sm:h-fit sm:w-1/2 sm:pl-24 sm:pr-0"
-    src="@/components/images/band.png"
-  />
-  <div class="mr-4 w-full text-center sm:w-1/2 sm:text-left">
-    <h2
-      class="mb-4 text-3xl font-bold leading-tight text-slate-50 md:text-4xl"
-    >
-      I'm Passionate About Music
-    </h2>
-    <p class="leading-relaxed text-slate-300">
-      I love making music! As a musician, I have played in  various bands and ensembles throughout my life...
-    </p>
-    <a
-      class="group relative mx-auto mt-8 inline-block font-semibold underline decoration-verdun-green decoration-2 underline-offset-8 transition  text-slate-200 hover:decoration-slate-200 sm:mx-0"
-      href="#"
-    >
-      About Me
-      
-      <svg
-        preserveAspectRatio="xMidYMid meet"
-        viewBox="0 0 24 24"
-        width="1.2em"
-        height="1.2em"
-        class="absolute bottom-0 -right-6 scale-x-0 transition group-hover:scale-x-100"
-      >
-        <path
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M5 12h14m-4 4l4-4m-4-4l4 4"
-        ></path>
-      </svg>
-    </a>
-  </div>
-</section>
+
 </template>
